08af00d18ed8f6ba21e812a65e5a199ffc613dd7
Tian Jianxiang <qwerqwer2900@icloud.com>
2022-06-25 23:27:55 +0800

    add

---
+++ 信号和槽.py
@@ -0,0 +1,35 @@
+import sys
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (QWidget, QLCDNumber, QSlider, QVBoxLayout, QApplication)
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        lcd = QLCDNumber(self)
+        sld = QSlider(Qt.Orientation.Horizontal, self)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lcd)
+        vbox.addWidget(sld)
+
+        self.setLayout(vbox)
+        sld.valueChanged.connect(lcd.display)
+
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle("Signal and slot")
+        self.show()
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
---
+++ 重新实现事件处理器.py
@@ -0,0 +1,28 @@
+import sys
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QWidget, QApplication
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle("Event handler")
+        self.show()
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key.Key_Escape.value:
+            self.close()
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
