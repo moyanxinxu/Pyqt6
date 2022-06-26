b3698e27cc0536b47b37ae9e9b2b406f063575a7
Tian Jianxiang <qwerqwer2900@icloud.com>
2022-06-26 20:13:57 +0800

    fix

---
+++ Py/QPixmap.py
@@ -0,0 +1,52 @@
+# file: pixmap.py
+#!/usr/bin/python
+
+"""
+ZetCode PyQt6 tutorial
+
+In this example, we display an image
+on the window.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+from PyQt6.QtWidgets import (QWidget, QHBoxLayout,
+        QLabel, QApplication)
+from PyQt6.QtGui import QPixmap
+import sys
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        hbox = QHBoxLayout(self)
+        pixmap = QPixmap(r"C:\Users\tianjianxiang\Desktop\新建位图图像.bmp")
+
+        lbl = QLabel(self)
+        lbl.setPixmap(pixmap)
+
+        hbox.addWidget(lbl)
+        self.setLayout(hbox)
+
+        self.move(300, 200)
+        self.setWindowTitle('Sid')
+        self.show()
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
