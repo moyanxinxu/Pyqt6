6ac76f5f2d4d7b9c26303d7c46fc64ebf9c87637
Tian Jianxiang <qwerqwer2900@icloud.com>
2022-06-26 19:26:51 +0800

    fix

--- 切换按钮.py
+++ 切换按钮.py
@@ -1,5 +1,18 @@
+# file: toggle_button.py
+#!/usr/bin/python
+
+"""
+ZetCode PyQt6 tutorial
+
+In this example, we create three toggle buttons.
+They control the background color of a QFrame.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
 from PyQt6.QtWidgets import (QWidget, QPushButton,
-                             QFrame, QApplication)
+        QFrame, QApplication)
 from PyQt6.QtGui import QColor
 import sys
 
@@ -8,11 +21,15 @@ class Example(QWidget):
 
     def __init__(self):
         super().__init__()
+
         self.initUI()
 
+
     def initUI(self):
+
         self.col = QColor(0, 0, 0)
-        redb = QPushButton('red', self)
+
+        redb = QPushButton('Red', self)
         redb.setCheckable(True)
         redb.move(10, 10)
 
@@ -26,17 +43,19 @@ class Example(QWidget):
 
         blueb = QPushButton('Blue', self)
         blueb.setCheckable(True)
-        blueb.move(10,110)
+        blueb.move(10, 110)
 
         blueb.clicked[bool].connect(self.setColor)
 
         self.square = QFrame(self)
         self.square.setGeometry(150, 20, 100, 100)
-        self.square.setStyleSheet("QWidget { background-color: %s }" % self.col.name())
+        self.square.setStyleSheet("QWidget { background-color: %s }" %
+                                  self.col.name())
 
         self.setGeometry(300, 300, 300, 250)
         self.setWindowTitle('Toggle button')
         self.show()
+
 
     def setColor(self, pressed):
 
@@ -46,17 +65,20 @@ class Example(QWidget):
             val = 255
         else:
             val = 0
+
         if source.text() == "Red":
             self.col.setRed(val)
-        elif source.text() == 'Green':
+        elif source.text() == "Green":
             self.col.setGreen(val)
         else:
             self.col.setBlue(val)
 
-        self.square.setStyleSheet("QFrame { background-color: %s }" % self.col.name())
+        self.square.setStyleSheet("QFrame { background-color: %s }" %
+                                  self.col.name())
 
 
 def main():
+
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec())
